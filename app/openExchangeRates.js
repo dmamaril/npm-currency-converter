25df2cb3f980bdc6191f30bc487cac793dbddc0b
Don Mamaril <mamarildon@gmail.com>
2014-08-27 18:33:23 -0700

    Compress DRY code

--- app/openExchangeRates.js
+++ app/openExchangeRates.js
@@ -6,22 +6,42 @@ var utils     = require('./libs/openExchangeUtils.js');
 
 var ratesPath = path.join(__dirname, './db/rates.txt');
 
+module.exports.fetchOptions = function (options) {
+  return new Promise(function (resolve, reject) {
+    var fetchOption = options.local ? module.exports.fetchLocalRates : module.exports.fetchLiveRates;
+    resolve(
+      fetchOption(options)
+        .then(function (rates) {
+          return module.exports.formatConversion(options, rates);
+        })
+    )
+  });
+}
 
-module.exports.fetchLiveRates = function () {
+module.exports.fetchLiveRates = function (options) {
   return new Promise(function (resolve, reject) {
     request.get(require('./config/openExchangeRates.js').URL, function (err, res) {
-      var rates = JSON.parse(res.body).rates;
-      fs.writeFileAsync(ratesPath, utils.update(rates))
-        .then(resolve);
+      fs.writeFileAsync(ratesPath, utils.update(JSON.parse(res.body).rates))
+        .then(function () {
+          resolve(module.exports.fetchLocalRates(options));
+        })
     });
   })
 };
 
-module.exports.fetchLocalRates = function (convertFrom, convertTo) {
+module.exports.fetchLocalRates = function (options) {
   return new Promise(function (resolve, reject) {
     fs.readFileAsync(ratesPath, 'utf-8')
       .then(function (contents) {
-        resolve(utils.parser(contents, convertFrom, convertTo));
+        resolve(utils.parser(contents, options.convertFrom, options.convertTo));
       });    
   })
 };
+
+module.exports.formatConversion = function (options, rates) {
+  var convertedRate = (options.amount / rates[0]['rate']) * rates[1]['rate'];
+  return {
+    'currency'  : options.convertTo,
+    'symbol'    : rates[1].symbol,
+    'amount'    : convertedRate.round(2) };
+ };
--- index.js
+++ index.js
@@ -2,25 +2,15 @@ var fs      = require('fs');
 var Promise = require('bluebird');
 var oxr     = require('./app/openExchangeRates.js');
 
-
 fs = Promise.promisifyAll(fs);
 
 module.exports.convert = function (options) {
   return new Promise(function (resolve, reject) {
     resolve(
-      oxr.fetchLiveRates()
-         .then(function () {
-            return oxr.fetchLocalRates(options.convertFrom, options.convertTo);
-         })
-         .then(function (rates) {
-            var convertedRate = (options.amount / rates[0]['rate']) * rates[1]['rate'];
-            return {
-              'currency'  : options.convertTo,
-              'symbol'    : rates[1].symbol,
-              'amount'    : convertedRate.round(2)
-            }
+      oxr.fetchOptions(options)
+         .then(function (converted) {
+          return converted;
          })
     )
   })
 };
-
