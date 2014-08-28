3ab91334867280e3c4040bb006b5407dde3eaba9
Don Mamaril <mamarildon@gmail.com>
2014-08-28 11:13:24 -0700

    Resolve proxy

--- index.js
+++ index.js
@@ -2,35 +2,31 @@ var fs          = require('fs');
 var Promise     = require('bluebird');
 var oxr         = require('./app/openExchangeRates.js');
 var currencies  = require('./app/libs/currencies.js');
-var OXR_KEY     = require('./app/libs/openExchangeRatesKey.js');
 
-fs = Promise.promisifyAll(fs);
-
-var currency_converter = {};
 
 currency_converter.convert = function (options) {
   return new Promise(function (resolve, reject) {
-    resolve(
-      oxr.fetchOptions(options)
-         .then(function (rates) {
-            return oxr.formatConversion(options, rates);
-         })
+    resolve(oxr.createProxy('formatConversion', options)
+      // oxr.fetchOptions(options)
+      //    .then(function (rates) {
+      //       return oxr.formatConversion(options, rates);
+      //    })
     );
   });
 };
 
 currency_converter.rates = function (options) {
   return new Promise(function (resolve, reject) {
-    resolve(
-      oxr.fetchOptions(options)
-         .then(function (rates) {
-            return oxr.formatConversionRate(options, rates);
-         })
+    resolve(oxr.createProxy('formatConversionRate', options)
+      // oxr.fetchOptions(options)
+      //    .then(function (rates) {
+      //       return oxr.formatConversionRate(options, rates);
+      //    })
     );
   });
 };
 
-currency_converter.verifyInput = function (input) {
+currency_converter.verifyInput = function (amount, convertFrom, convertTo) {
   var checkAmount   = true;
   var checkCurrency = currencies.hasOwnProperty(input.convertFrom) && 
                       currencies.hasOwnProperty(input.convertTo);
@@ -40,7 +36,9 @@ currency_converter.verifyInput = function (input) {
   return checkAmount && checkCurrency;
 };
 
-module.exports = function (openExchangeRatesKey) {
-  OXR_KEY.URL += openExchangeRatesKey;
+module.exports = function (configs) {
+  oxr.URL += configs.CLIENTKEY;
+  oxr.init(configs.fetchInterval);
+
   return currency_converter;
 };
